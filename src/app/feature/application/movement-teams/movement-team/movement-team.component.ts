bf26937465c901ab3d1d6420f16c85b3b4cae3e5
Carlos <carlossilvestri9@gmail.com>
2021-10-27 21:11:52 -0400

    Get teamusers

--- src/app/feature/application/movement-teams/movement-team/movement-team.component.ts
+++ src/app/feature/application/movement-teams/movement-team/movement-team.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TeamService } from 'src/app/shared/services/team/team.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HelpersService } from '../../../../shared/services/helpers/helpers.service';
+import { Team } from 'src/app/core/interfaces/team-requests.interface';
+import { UserService } from '../../../../shared/services/user/user.service';
+import { User } from '../../../../core/interfaces/user-requests.interface';
+import { TeamUserService } from '../../../../shared/services/team_user/team-user.service';
+import { TeamUser } from '../../../../core/interfaces/team_user-requests.interface';
 
 @Component({
   selector: 'app-movement-team',
@@ -6,10 +15,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movement-team.component.scss']
 })
 export class MovementTeamComponent implements OnInit {
+  
+  loadingUsers: boolean = false;
+  loadingTeams: boolean = false;
+  loadingTeamUser: boolean = false;
+  teams: Team[];
+  users: User[];
+  id_team_user: number = 0;
+  forma: FormGroup;
+  team_users: TeamUser[];
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    public teamService: TeamService,
+    public userService : UserService,
+    public teamUserService : TeamUserService,
+    public router: Router,
+    private route: ActivatedRoute,
+    public helpersService: HelpersService
+  ) {}
 
   ngOnInit(): void {
+    this.id_team_user = this.route.snapshot.params['id'];
+    console.log('id_team_user ', this.id_team_user);
+    this.fetchFields();
   }
-
+  fetchFields(): void {
+    this.fetchTeams();
+    if (this.id_team_user) {
+      this.fetchTeams();
+      this.fetchUsers();
+    } else {
+      this.initFormCreate();
+    }
+  }
+  /*
+  // TODO - Get TeamUser by id.
+  fetchTeamUser(): void {
+    this.loadingTeamUser = true;
+    this.teamUserService.get(this.id_team_user).subscribe((resp) => {
+      this.team_users = resp.;
+      console.log("account ", this.account);
+      this.initFormEdit();
+    });
+  }
+  */
+  fetchTeams() : void {
+    this.loadingTeams = true;
+    this.teamService.getTeams().subscribe((resp) => {
+      this.teams = resp.teams;
+      this.loadingTeams = false;
+    });
+  }
+  fetchUsers() : void {
+    this.loadingUsers = true;
+    this.userService.getUsers(0).subscribe((resp) => {
+      this.users = resp.users;
+      this.loadingUsers = false;
+    });
+  }
+  
+  initFormCreate() {
+    this.forma = this.fb.group({
+      id_user_f: [0, [Validators.required, Validators.minLength(1)]],
+      id_team_f: [0, [Validators.required, Validators.min(1)]],
+      beggining_date: ['', [Validators.required]],
+      ending_date: ['', [Validators.required]],
+    });
+    this.loadingTeamUser = false;
+  }
 }
--- src/app/feature/application/movement-teams/movement-teams.component.ts
+++ src/app/feature/application/movement-teams/movement-teams.component.ts
@@ -1,22 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeamUser } from 'src/app/core/interfaces/team_user-requests.interface';
+import { HelpersService } from 'src/app/shared/services/helpers/helpers.service';
+import { TeamUserService } from 'src/app/shared/services/team_user/team-user.service';
 
 @Component({
   selector: 'app-movement-teams',
   templateUrl: './movement-teams.component.html',
-  styleUrls: ['./movement-teams.component.scss']
+  styleUrls: ['./movement-teams.component.scss'],
 })
 export class MovementTeamsComponent implements OnInit {
-
+  /* =============================
+                VARIABLES
+     =============================
+  */
   filterBy: string = 'TEAM';
-  constructor() { }
+  team_users: TeamUser[];
+  desde: number = 0;
+  loadingTeamUsers: boolean = false;
+  thereWasAnError: boolean = false;
+  constructor(
+    public router: Router,
+    public teamUserService: TeamUserService,
+    public helpersService: HelpersService
+  ) {}
 
   ngOnInit(): void {
+    this.fetchTeamUsers();
+
   }
-  changeFilter(text : string): void {
+  /**
+   * Get the team_users, calling the API.
+   */
+  fetchTeamUsers(): void {
+    this.loadingTeamUsers = true;
+    this.teamUserService.getTeamUsers(this.desde).subscribe(
+      (resp) => {
+        this.loadingTeamUsers = false;
+        this.thereWasAnError = false;
+        this.team_users = resp.team_users;
+        console.log('resp ', resp);
+      },
+      (err) => {
+        console.log('err ', err);
+        this.loadingTeamUsers = false;
+        this.thereWasAnError = true;
+      }
+    );
+  }
+  changeFilter(text: string): void {
     this.filterBy = text;
   }
   onChangeRadioButton(event: any) {
     this.changeFilter(event.target.value);
   }
-
+  updatePage(event: number) {
+    // console.log('Evento updatePage llamado: ', event);
+    this.desde = event;
+    this.fetchTeamUsers();
+  }
 }
